5ff099f8499519bdc304f3468b9d37bbc795cc5b
ty64q <71687806+ty64q@users.noreply.github.com>
2020-11-17 12:23:36 +0800

    跳不起來

---
+++ 2D_Midterm_106051044/Assets/C#/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   public void BackToMenu()
+    {
+        SceneManager.LoadScene("開始畫面");
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ 2D_Midterm_106051044/Assets/C#/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public Text textCount;
+    public int count;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.name == "傳送門")
+        {
+            final.SetActive(true);
+        }
+
+        if (collision.tag == "盾牌")
+        {
+            Destroy(collision.gameObject);
+
+            count++;
+            textCount.text = "SHIELD: " + count;
+        }
+    }
+}
